9efc4ecffcd0311b559c0c6d5f8d3977a3ea9cbb
hi-lufz1 <latifufauzi@gmail.com>
2023-05-19 13:05:46 +0700

    8

--- singlelinkedlist/singlelinkedlist.cpp
+++ singlelinkedlist/singlelinkedlist.cpp
@@ -14,37 +14,41 @@ void addNode() {
 	int nim;
 	string nama;
 	Node* nodeBaru = new Node();
-	cout << "Masukikan NIM.";
+	cout << "Masukan NIM: ";
 	cin >> nim;
-	cout << "Masukkan Nama : ";
+	cout << "Masukan Nama: ";
 	cin >> nama;
 	nodeBaru->noMhs = nim;
 	nodeBaru->name = nama;
 
 	if (START == NULL || nim <= START->noMhs) {
-		if (START != NULL && nim == START->noMhs) {
-			cout << "NIM sudah ada " << endl;
+		if (START != NULL && nim == START->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
 			return;
 		}
 
-		Node* current = START;
-		Node* previous = START;
-
-		while ((current != NULL) && (nim >= current->noMhs))
-		{
-			if (nim == current->noMhs) {
-				cout << "Nim sudah ada" << endl;
-				return;
-			}
-
-			previous = current;
-			current = current->next;
-		}
-
-		nodeBaru->next = current;
-		previous->next = nodeBaru;
+		nodeBaru->next = START;
+		START = nodeBaru;
+		return;
 	}
+
+	Node* previous = START;
+	Node* current = START;
+
+	while ((current != NULL) && (nim >= current->noMhs))
+	{
+		if (nim == current->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+	}
+
+	nodeBaru->next = current;
+	previous->next = nodeBaru;
 }
+
 
 bool searchNode(int nim, Node* current, Node* previous) {
 	previous = START;
