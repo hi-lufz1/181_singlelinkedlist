2608b3ba081a1a265df090d8773efa7ae1fa11b7
hi-lufz1 <latifufauzi@gmail.com>
2023-05-19 14:59:23 +0700

    14

--- singlelinkedlist/singlelinkedlist.cpp
+++ singlelinkedlist/singlelinkedlist.cpp
@@ -100,8 +100,13 @@ void traverse() {
 		system("pause");
 		system("cls");
 		return;
-
-
-
+	}
+	else {
+		Node* currentNode = START;
+		while (currentNode != NULL) {
+			cout << "NIM: " << currentNode->noMhs << ", Nama: " << currentNode->name << endl;
+			currentNode = currentNode->next;
+		}
+	 }
 
 	}
